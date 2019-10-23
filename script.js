0fa91976289312e0b196f703f6b88451c0c981ed
Hellen Ward <hellenwellsward@gmail.com>
2019-10-23 07:20:30 +0100

    Well this got messy.  Saving progress...

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var Dracula = require('graphdracula');
+var Graph = Dracula.Graph;
+var Renderer = Dracula.Renderer.Raphael;
+var Layout = Dracula.Layout.Spring;
+
 var userName = document.getElementById('userName');
 var partner1 = document.getElementById('partner1Name');
 var partner2 = document.getElementById('partner2Name');
@@ -8,6 +13,15 @@ submitButton.addEventListener('click', function() {
   var partner1Name = partner1.value;
   var partner2Name = partner2.value;
   var partner3Name = partner3.value;
-  alert('Hello ' + storedUserName + '! You are dating ' + partner1Name + ', ' +
-  partner2Name + ', and ' + partner3Name);
 });
+
+var g = new Graph();
+g.addEdge(storedUserName, partner1Name);
+g.addEdge(storedUserName, partner2Name);
+g.addEdge(storedUserName, partner3Name);
+
+var layouter = new Dracula.Layout.Spring(g);
+layouter.layout();
+
+var renderer = new Dracula.Renderer.Raphael('canvas', g, 400, 300);
+renderer.draw();

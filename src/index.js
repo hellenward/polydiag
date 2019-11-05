9ce7698e859b2c60f852fa1fc6e97a2586b74b98
Hellen Ward <hellenwellsward@gmail.com>
2019-11-05 07:02:42 +0000

    Created add people buttons, created a function that currently just comes up with an alert.  Need to append HTMLto increase fields.

--- src/index.js
+++ src/index.js
@@ -4,36 +4,38 @@ var Graph = Dracula.Graph
 var Renderer = Dracula.Renderer.Raphael
 var Layout = Dracula.Layout.Spring
 
-
 window.onload = function() {
 
   var userName = document.getElementById('userName');
   var partner1 = document.getElementById('partner1Name');
-  var partner2 = document.getElementById('partner2Name');
-  var partner3 = document.getElementById('partner3Name');
+  var addPeopleButtons = document.getElementsByClassName("addPeople");
+  console.log(addPeopleButtons);
   var submitButton = document.getElementById('submitButton');
   submitButton.addEventListener('click', function(event) {
     event.preventDefault();
-    drawGraph(userName.value, partner1.value, partner2.value, partner3.value);
-    /*
-    storedUserName = userName.value;
-    partner1Name = partner1.value;
-    partner2Name = partner2.value;
-    partner3Name = partner3.value;
-    */
+    drawGraph(userName.value, partner1.value);
   });
 
+  for (let item of addPeopleButtons) {
+    item.addEventListener('click', function() {
+      alert('hello!');
+    })
+  }
 
 }
 
-function drawGraph(userName, partner1, partner2, partner3) {
-  var graph = new Graph()
+function removeElement(element) {
+  var element = document.getElementsByTagName('svg');
+  for (let item of element) {
+    item.remove();
 
+  }
+}
+
+function drawGraph(userName, partner1) {
+  var graph = new Graph();
+  removeElement();
   graph.addEdge(userName, partner1)
-  graph.addEdge(partner1, partner2)
-  graph.addEdge(partner1, partner3)
-  graph.addEdge(partner3, userName)
-  graph.addEdge(partner2, userName)
 
   var layout = new Layout(graph)
   var renderer = new Renderer('#paper', graph, 400, 300)
